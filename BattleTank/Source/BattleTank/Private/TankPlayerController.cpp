2f8e16decd0746f01638a366c405fc562dae5858
Jib333 <zfinman333@yahoo.com>
2019-10-18 06:46:11 -0600

    Added a tick function for when we start aiming through the crosshair to whatever is behind it.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -17,10 +17,24 @@ void ATankPlayerController::BeginPlay()
 	{
 		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *(ControlledTank->GetName()));
 	}
-	
+
+}
+
+void ATankPlayerController::Tick(float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+	AimTowardsCrosshair();
 }
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+	//Get World location if linetrace through crosshair
+	//if it hits the landscape
+		//tell controlled tank to aim at this point
 }
